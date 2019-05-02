5aa9f2daffe0b161277f51bcf76b352ad409bbf7
Chikipiki96 <imtiaz.ayon96@gmail.com>
2019-05-02 20:52:03 +0600

    started working on file download! Problem in file extension

--- file transfer test/app.js
+++ file transfer test/app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const bodyParser= require('body-parser')
 const multer = require('multer');
 const app = express();
+const path = require('path');
 app.use(bodyParser.urlencoded({extended: true}));
 
-var upload = multer({ dest: 'uploads/' });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  }
+})
+ 
+var upload = multer({ storage: storage })
 
 app.get('/', function(req, res) {
     res.render("home.ejs");   
@@ -21,6 +28,12 @@ app.post("/uploadfile", upload.single('myFile'),function(req, res){
 	}
 });
 
+app.get('/download/:file(*)',(req, res) => {
+  var file = req.params.file;
+  var fileLocation = path.join('./uploads',file);
+  console.log(fileLocation);
+  res.download(fileLocation, file); 
+});
 
 app.listen(3000, function(){
 	console.log('Server started on port 3000')
