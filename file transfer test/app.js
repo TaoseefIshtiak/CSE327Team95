2f79dae05095075b328ca9add1ea3ce63421837f
Chikipiki96 <imtiaz.ayon96@gmail.com>
2019-05-02 20:18:57 +0600

    file upload working in test app

--- file transfer test/app.js
+++ file transfer test/app.js
@@ -2,16 +2,23 @@ const express = require('express')
 const bodyParser= require('body-parser')
 const multer = require('multer');
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
- 
+app.use(bodyParser.urlencoded({extended: true}));
 
+var upload = multer({ dest: 'uploads/' });
 
 app.get('/', function(req, res) {
     res.render("home.ejs");   
 });
  
-app.post("/uploadfile", function(req, res){
-	
+app.post("/uploadfile", upload.single('myFile'),function(req, res){
+	if (req.file) {
+        console.log('Uploading file...');
+        var filename = req.file.filename;
+        console.log(filename);
+	}
+	else{
+		console.log('Failed to upload!');
+	}
 });
 
 
